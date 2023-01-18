5ef6b8ed2667dfeb49e40d23dd4f681a2f52e158
masum9500 <masumbillah7286@gmail.com>
2023-01-18 22:54:30 +0600

    Add Shipping Area from Backend

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\CouponController;
+use App\Http\Controllers\Backend\ShippingAreaController;
 
 
 use App\Http\Controllers\Frontend\IndexController;
@@ -247,6 +248,49 @@ Route::post('/store', [CouponController::class, 'CouponStore'])->name('coupon.st
 Route::get('/edit/{id}', [CouponController::class, 'CouponEdit'])->name('coupon.edit');
 Route::post('/update/{id}', [CouponController::class, 'CouponUpdate'])->name('coupon.update');
 Route::get('/delete/{id}', [CouponController::class, 'CouponDelete'])->name('coupon.delete');
+
+});
+
+
+// Admin Shipping All Routes 
+
+Route::prefix('shipping')->group(function(){
+
+Route::get('/division/view', [ShippingAreaController::class, 'DivisionView'])->name('manage-division');
+Route::post('/division/store', [ShippingAreaController::class, 'DivisionStore'])->name('division.store');
+Route::get('/division/edit/{id}', [ShippingAreaController::class, 'DivisionEdit'])->name('division.edit');
+Route::post('/division/update/{id}', [ShippingAreaController::class, 'DivisionUpdate'])->name('division.update');
+Route::get('/division/delete/{id}', [ShippingAreaController::class, 'DivisionDelete'])->name('division.delete');
+
+
+
+
+// Ship District 
+Route::get('/district/view', [ShippingAreaController::class, 'DistrictView'])->name('manage-district');
+
+Route::post('/district/store', [ShippingAreaController::class, 'DistrictStore'])->name('district.store');
+
+Route::get('/district/edit/{id}', [ShippingAreaController::class, 'DistrictEdit'])->name('district.edit');
+
+Route::post('/district/update/{id}', [ShippingAreaController::class, 'DistrictUpdate'])->name('district.update');
+
+Route::get('/district/delete/{id}', [ShippingAreaController::class, 'DistrictDelete'])->name('district.delete');
+
+
+
+
+// Ship State 
+Route::get('/state/view', [ShippingAreaController::class, 'StateView'])->name('manage-state');
+
+Route::post('/state/store', [ShippingAreaController::class, 'StateStore'])->name('state.store');
+
+Route::get('/state/edit/{id}', [ShippingAreaController::class, 'StateEdit'])->name('state.edit');
+
+Route::post('/state/update/{id}', [ShippingAreaController::class, 'StateUpdate'])->name('state.update');
+
+Route::get('/state/delete/{id}', [ShippingAreaController::class, 'StateDelete'])->name('state.delete');
+
+
 
 });
 
