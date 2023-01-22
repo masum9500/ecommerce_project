9a5baade42120a3126ec6c66e2732a106c255b2b
masum9500 <masumbillah7286@gmail.com>
2023-01-22 15:31:06 +0600

    View Order and Cash on Delivery

--- resources/views/frontend/profile/change_password.blade.php
+++ resources/views/frontend/profile/change_password.blade.php
@@ -7,16 +7,7 @@ $user = DB::table('users')->where('id', Auth::user()->id)->first();
 <div class="body-content">
     <div class="container">
         <div class="row">
-            <div class="col-md-2"><br>
-                <img class="card-img-top" style="border-radius: 50%;" src="{{ (!empty($user->profile_photo_path)) ? url('upload/user_images/'.$user->profile_photo_path):url('upload/no_image.jpg') }}" height="100%" width="100%"><br><br>
-
-                <ul class="list-group list-group-flush">
-                    <a href="{{ route('dashboard')}}" class="btn btn-primary btn-block btn-sm">Home</a>
-                    <a href="{{ route('user.profile') }}" class="btn btn-primary btn-block btn-sm">Profile Update</a>
-                    <a href="{{ route('change.password') }}" class="btn btn-primary btn-block btn-sm">Change Password</a>
-                    <a href="{{ route('user.logout') }}" class="btn btn-danger btn-block btn-sm">Logout</a>
-                </ul>
-            </div>
+            @include('frontend.common.user_sidebar')
             <div class="col-md-2">
                 
             </div>
